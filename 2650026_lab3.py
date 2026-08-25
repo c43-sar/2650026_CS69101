84ab0c31c96ce8226a8eb4622986e9a7e31a4ac5
Soumyajit Kolay <95214806+c43-sar@users.noreply.github.com>
2026-08-25 11:41:20 +0530

    Added Height and BF logic to lab3

--- 2650026_lab3.py
+++ 2650026_lab3.py
@@ -12,3 +12,34 @@ except:
     sys.exit("[ERROR] Invalid data")
 
 print(arr0)
+
+# Construct a balanced AVL tree by inserting these elements one by one, performing the
+# necessary LL, RR, LR, RL rotations to maintain balance after every insertion.
+
+class AVL_Node:
+    def __init__(self):
+        self.lchild = None
+        self.rchild = None
+        self.value = None
+        self.parent = None
+        self.height = None
+        self.balance_factor = None
+
+def AVL_get_height(node: AVL_Node) -> int:
+    if not node:
+        return 0
+
+    left = right = 0
+    if node.lchild:
+        left = node.lchild.height
+    if node.rchild:
+        right = node.rchild.height
+
+    node.height = 1 + max(left.height, right.height) # type: ignore
+    return node.height
+
+def AVL_get_balance_factor(node: AVL_Node) -> int:
+    if not node:
+        return 0
+    return AVL_get_height(node.lchild) - AVL_get_height(node.rchild) # type: ignore
+
